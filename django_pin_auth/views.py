c7c5ec2dd5ed2cf138073d7c5bbc8e214ab606e7
matiboy <mathieu.chauvinc@gmail.com>
2017-09-29 06:58:23 +0800

    feat(verification): Display enter pin view using form
    - Enter pin view
    - Template with expected login view in mind
    - For with split input
    widget
    
    TODO: check how the value comes out of the split input

--- django_pin_auth/views.py
+++ django_pin_auth/views.py
@@ -9,7 +9,7 @@ from .models import (
 	SingleUseToken,
 )
 
-from .forms import RegisterForm
+from .forms import RegisterForm, PinForm
 
 
 class RegisterView(FormView):
@@ -19,13 +19,19 @@ class RegisterView(FormView):
 
     def form_valid(self, form):
         """Handle valid form."""
+        self.request.session['email'] = form.cleaned_data['email']
         form.save()
         form.send_email(self.request)
         return redirect('django_pin_auth:enter_pin')
 
 
-# TODO
 class EnterPinView(FormView):
     """Page where users enter their pin."""
-    template_name = 'django_pin_auth/register.html'
-    form_class = RegisterForm
+    template_name = 'django_pin_auth/enter_pin.html'
+    form_class = PinForm
+    
+    def get_context_data(self):
+        """Get context for pin verification view."""
+        context = super().get_context_data()
+        context['email'] = self.request.session.get('email')
+        return context
